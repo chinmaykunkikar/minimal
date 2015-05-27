c565b8745dddb22ab18e6d084acee7c8021b15ed
dark-falcon <chinmay.kunkikar@gmail.com>
2015-05-28 02:38:07 +0530

    Comment out unnecessary lunch combos

--- device/sony/pepper/vendorsetup.sh
+++ device/sony/pepper/vendorsetup.sh
@@ -1,4 +1,6 @@
-add_lunch_combo cm_pepper-user
-add_lunch_combo cm_pepper-userdebug
-add_lunch_combo cm_pepper-eng
+# No need of these lunch combos now
+
+# add_lunch_combo cm_pepper-user
+# add_lunch_combo cm_pepper-userdebug
+# add_lunch_combo cm_pepper-eng
 
