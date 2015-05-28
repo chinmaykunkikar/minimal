43665e335507fa84f2dd80cd696b8a8d08ee26c7
dark-falcon <chinmay.kunkikar@gmail.com>
2015-05-28 18:27:56 +0530

    Scripts always make any job easy

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ccache
+export USE_CCACHE=1
+export CCACHE_NLEVELS=4
+export CCACHE_DIR=/home/darkfalcon/ccache/minos
+# Build
+. build/envsetup.sh
+export KBUILD_BUILD_USER=darkfalcon
+export KBUILD_BUILD_HOST=omen
+lunch minimal_pepper-userdebug
+export USE_PREBUILT_CHROMIUM=1
+make clean && make clobber
+#export CHANGELOG=true
+make
