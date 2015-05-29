789dc7b04f273f263a0044c865a8a4659736a481
dark-falcon <chinmay.kunkikar@gmail.com>
2015-05-29 22:37:08 +0530

    Update

--- compile.sh
+++ compile.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # ccache
-export USE_CCACHE=1
-export CCACHE_NLEVELS=4
-export CCACHE_DIR=/home/darkfalcon/ccache/minos
+export USE_CCACHE=1;
+export CCACHE_NLEVELS=4;
+export CCACHE_DIR=/home/darkfalcon/ccache/minos;
 # Build
-. build/envsetup.sh
-export KBUILD_BUILD_USER=darkfalcon
-export KBUILD_BUILD_HOST=omen
-lunch minimal_pepper-userdebug
-export USE_PREBUILT_CHROMIUM=1
-make clean && make clobber
-#export CHANGELOG=true
-make -j8
+. build/envsetup.sh;
+export KBUILD_BUILD_USER=darkfalcon;
+export KBUILD_BUILD_HOST=omen;
+lunch minimal_pepper-userdebug;
+export USE_PREBUILT_CHROMIUM=1;
+make clean && make clobber;
+make -j8 otapackage;
