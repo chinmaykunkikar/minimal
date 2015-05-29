81f98a32eabb3a5cb6899de80e02304aea95bc9a
dark-falcon <chinmay.kunkikar@gmail.com>
2015-05-29 17:04:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,18 +24,26 @@ cp "build.sh" "$MINIMAL";
 cd $DPEPPER;
 cp "minimal.dependencies" "$MINIMAL/device/sony/pepper/";
 rm "$MINIMAL/device/sony/pepper/vendorsetup.sh" && cp "vendorsetup.sh" "$MINIMAL/device/sony/pepper/";
+echo "'minimal.dependencies' & 'vendorsetup.sh' copied to path '/device/sony/pepper'";
 if [ -f "$MINIMAL/device/sony/pepper/cm.mk" ]; then
 rm "$MINIMAL/device/sony/pepper/cm.mk"
+else
+echo "No cm.mk found, skipping"
 fi
 
 cd $LMANIFESTS;
 rm "$MINIMAL/.repo/local_manifests/local_manifest.xml" && cp "local_manifest.xml" "$MINIMAL/.repo/local_manifests/";
+echo "'local_manifest.xml' copied to path '/.repo/local_manifests/'";
 
 cd $VMINIMAL;
 rm "$MINIMAL/vendor/minimal/vendorsetup.sh" && cp "vendorsetup.sh" "$MINIMAL/vendor/minimal/";
+echo "'vendorsetup.sh' copied to path '/vendor/minimal/'";
 
 cd $VPRODUCT;
 rm "$MINIMAL/vendor/minimal/products/AndroidProducts.mk" && cp "AndroidProducts.mk" "$MINIMAL/vendor/minimal/products/";
 cp "minimal_pepper.mk" "$MINIMAL/vendor/minimal/products/";
+echo "'AndroidProducts.mk' & 'minimal_pepper.mk' copied to path '/vendor/minimal/products/'";
+
+echo "Done!"
 
 exit
