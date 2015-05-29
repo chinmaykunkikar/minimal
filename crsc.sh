9f462f953c2ebcdc58c5f84b45d891dde1ce60c1
Chinmay <chinmay.kunkikar@gmail.com>
2015-05-29 22:49:12 +0530

    Hope it'll work for not

--- crsc.sh
+++ crsc.sh
@@ -1,1 +1,1 @@
-bash clean.sh && bash reset.sh && bash setup.sh && repo sync -j8 && bash compile.sh;
+bash clean.sh && bash reset.sh && bash ../working/minos_pepper/setup.sh && repo sync -j8 && bash compile.sh;
