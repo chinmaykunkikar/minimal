ead44339299359a6ec7c54470148836cf3ef6760
Chinmay <dark-falcon@users.noreply.github.com>
2015-09-19 17:57:42 +0530

    build.sh: Few tweaks to make stuff look nice

--- build.sh
+++ build.sh
@@ -6,23 +6,15 @@
 #Help menu
 case $1 in
   -h|--help )
-      echo -e "Minimal OS build script"
-      echo -e "Usage:\n     ./build.sh [PRODUCT_NAME] [-sc] [JOBS]"
+      echo -e "\n\nMinimal OS build script"
+      echo -e "Usage:\n     ./build.sh [PRODUCT_NAME] [MODE] [JOBS]"
       echo -e "PRODUCT_NAME   -   Product you're currently building for"
-      echo -e "[sc]           -   Specify the mode. -s is for repo sync and -c is for clean build."
+      echo -e "MODE           -   Specify the mode. -s is for repo sync and -c is for clean build."
       echo -e "                   Using -sc whill perform a repo sync as well as a clean build."
       echo -e "JOBS           -   Number of CPU jobs/threads you want the build to utilize"
-      echo -e "-h | --help    -   Show this help and exit"
+      echo -e "-h | --help    -   Show this help and exit\n\n"
       exit
 esac
-
-
-# Define everything here
-PRODUCT_NAME="$1"
-MODE="$2"
-JOBS="$3"
-HOST=$(echo $HOSTNAME)
-USER=$(whoami)
 
 
 #Sanity checks
@@ -32,23 +24,40 @@ then
   exit
 fi
 
-if [ !"$PRODUCT_NAME" ]
+if [ ! "$1" ]
 then
   echo -e "No product name supplied."
   echo -e "Please enter ./build.sh $PRODUCT_NAME to run script. \nEnter ./build.sh --help for usage and more options"
   exit
+  else
+    PRODUCT_NAME="$1"
 fi
 
-if [ !"$JOBS" ]
+if [ ! "$2" ]
 then
-  echo -e "Warning: Number of jobs not provided"
-  echo -e "The script will run further tasks using default jobs"
+  echo -e "No mode supplied."
   echo -e "Enter ./build.sh --help for usage and more options"
+  else
+    if [ ! "$3" ]
+    then
+      echo -e "Warning: Number of jobs not provided"
+      echo -e "The script will run further tasks using default jobs"
+      echo -e "Enter ./build.sh --help for usage and more options"
+      JOBS="$2"
+    fi
+  MODE="$2"
+  JOBS="$3"
 fi
+
+
+# Define everything here
+HOST=$(echo $HOSTNAME)
+USER=$(whoami)
+
 
 # Optimizations
 optimizations() {
-  flags=( 'echo "Proceeding without using optimizaions"' 'export USE_O3_OPTIMIZATIONS=true' 'export FLOOP_NEST_OPTIMIZE=true' 'export FFAST_MATH=true' 'export STRICT_ALIASING=true' 'export KRAIT_TUNINGS=true' 'export ENABLE_GCCONLY=true' 'export GRAPHITE_OPTS=true' 'export USE_HOST_4_8=true' 'export SUPPRES_UNUSED_WARNING=true' )
+  flags=( 'echo -e \nProceeding without using optimizaions' 'export USE_O3_OPTIMIZATIONS=true' 'export FLOOP_NEST_OPTIMIZE=true' 'export FFAST_MATH=true' 'export STRICT_ALIASING=true' 'export KRAIT_TUNINGS=true' 'export ENABLE_GCCONLY=true' 'export GRAPHITE_OPTS=true' 'export USE_HOST_4_8=true' 'export SUPPRES_UNUSED_WARNING=true' )
   echo -e "Select optimizations:"
   echo -e "1. USE_O3_OPTIMIZATIONS"
   echo -e "2. FLOOP_NEST_OPTIMIZE"
@@ -60,6 +69,7 @@ optimizations() {
   echo -e "8. USE_HOST_4_8"
   echo -e "9. SUPPRES_UNUSED_WARNING"
   echo -e "0. None"
+  echo -e ""
   read -e options
   for index in $options
   do
@@ -70,10 +80,10 @@ optimizations() {
 # repo sync
 if [ $MODE == "-s" ] || [ $MODE == "-sc" ]
 then
-      echo -e "Repo syncing..."
+      echo -e "\nRepo syncing..."
       repo sync "$JOBS"
   else
-      echo -e "Proceeding without repo syncing..." 
+      echo -e "\nProceeding without repo syncing..." 
 fi
 
 
@@ -82,16 +92,16 @@ echo -e "\nYour user is $USER and host is $HOST\n"
 
 
 # Start the actual build
-echo -e "Setting up the environment..."
+echo -e "Setting up the environment...\n"
 sleep 3
+source build/envsetup.sh
 lunch minimal_"$PRODUCT_NAME"-userdebug
-optimiztions()
+optimizations;
 export KBUILD_BUILD_USER="$USER"
 export KBUILD_BUILD_HOST="$HOST"
-export USE_PREBUILT_CHROMIUM=true;
+export USE_PREBUILT_CHROMIUM=true
 export CHANGELOG=true
-rm $OUT/system/build.prop;
-echo -e "Environment set up."
+echo -e "\nEnvironment set up.\n"
 
 # Release build
 echo -e "Is this a release build? [Default=No]"
@@ -109,10 +119,13 @@ case $release in
 esac
 
 # Clean build
-echo -e "\n\nStarting build for $PRODUCT_NAME\n\n"
-if [ $MODE == "-c" ] || [ $MODE == "-sc" ]; then
+echo -e "Starting build for $PRODUCT_NAME\n\n"
+sleep 3
+if [ $MODE == "-c" ] || [ $MODE == "-sc" ]
+then
        echo -e "Cleaning up out folder..."
        mka clean && mka bacon "$JOBS"
     else
+      rm $OUT/system/build.prop
       mka bacon "$JOBS"
 fi
