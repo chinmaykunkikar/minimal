09a28d8610f2f752b06e2413286b5b9d7d1bcce9
Chinmay <dark-falcon@users.noreply.github.com>
2016-02-08 15:04:52 +0530

    timeconst_fix.sh: Fix script

--- timeconst_fix.sh
+++ timeconst_fix.sh
@@ -3,30 +3,29 @@
 # Definitions
 ENVSETUP=build/envsetup.sh
 KERNELSRC=kernel/motorola/msm8916
-TIMECONSTBCCOMMIT=70730bca1331fc50c3caacaea00439de1325bd6e
-TIMECONSTWARNINGCOMMIT=7b4a6e771369f394726154db0bc465acfd00a1a3
-REMOTECHECK=git remote | grep minimal
+TIMECONSTBCCOMMIT='70730bca1331fc50c3caacaea00439de1325bd6e'
+TIMECONSTWARNINGCOMMIT='7b4a6e771369f394726154db0bc465acfd00a1a3'
 REMOTEURL=https://github.com/MinimalOS-AOSP/kernel_huawei_angler
 
 echo -e "\nFixing timeconst error real quick\n"
 
-if [ ! -f $ENVSETUP ] 
+if [ ! -f $ENVSETUP ]
 then
   echo -e "The script is probably not in the parent source folder. \nExiting..."
   exit
 fi
 
+function cherry-pick() {
+  git fetch minimal mr1
+  git cherry-pick $TIMECONSTWARNINGCOMMIT
+}
+
 cd $KERNELSRC
 git revert $TIMECONSTBCCOMMIT
-if [ $($REMOTECHECK) == "minimal" ]
+if [ $(git remote | grep minimal) == "minimal" ]
 then
   cherry-pick
 else
   git remote add minimal $REMOTEURL
   cherry-pick
 fi
-
-cherry-pick() {
-  git fetch minimal mr1
-  git cherry-pick $TIMECONSTWARNINGCOMMIT
-}
